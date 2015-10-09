9d4baed4f970cc93e23ef092f567d376ad7ec78a
choijaewoong <jwchoi0129@gmail.com>
2015-10-09 14:59:31 +0900

    Tab with ViewPager

---
+++ app/src/main/java/com/example/androidchoi/kakaopractice/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.androidchoi.kakaopractice;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG_INDEX = "tagIndex";
+    TabHost mTabHost;
+    ViewPager mPager;
+    TabsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mTabHost = (TabHost)findViewById(R.id.tabHost);
+        mTabHost.setup(); //등록
+        mPager = (ViewPager)findViewById(R.id.pager);
+        mAdapter = new TabsAdapter(this, getSupportFragmentManager(), mTabHost, mPager);
+
+        mAdapter.addTab(mTabHost.newTabSpec("tab1").setIndicator("TAB1"), FriendsListFragment.class, null);
+        mAdapter.addTab(mTabHost.newTabSpec("tab2").setIndicator("TAB2"), ChatListFragment.class, null);
+        mAdapter.addTab(mTabHost.newTabSpec("tab3").setIndicator("TAB3"), NewsFragment.class, null);
+        mAdapter.addTab(mTabHost.newTabSpec("tab4").setIndicator("TAB4"), MoreFragment.class, null);
+
+        if(savedInstanceState != null){
+            mTabHost.setCurrentTab(savedInstanceState.getInt(TAG_INDEX));
+            mAdapter.onRestoreInstanceState(savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        outState.putInt(TAG_INDEX, mTabHost.getCurrentTab());
+        mAdapter.onSaveInstanceState(outState);
+    }
+}
---
+++ app/src/main/java/com/example/androidchoi/kakaopractice/model/Friend.java
@@ -0,0 +1,14 @@
+package com.example.androidchoi.kakaopractice.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Choi on 2015-10-09.
+ */
+public class Friend {
+
+    Drawable profileImage;
+    String name;
+    String introMent;
+    String phoneNumber;
+}
